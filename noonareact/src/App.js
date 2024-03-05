b8ee2137d64eab0fc50c6536619c62e76948b117
Molkang <mitier26@gmail.com>
2024-03-06 02:17:08 +0900

    리엑트  컴포넌트 기초

--- noonareact/src/App.js
+++ noonareact/src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import Box from './component/Box';
+import { useState } from 'react';
 
 function App() {
-    return <div>여기에 하는 것인가?</div>;
+    let counter = 0;
+    const [counter2, setCounter2] = useState(0);
+
+    const increase = () => {
+        counter++;
+        setCounter2(counter2 + 1);
+        console.log(counter, counter2);
+    };
+    return (
+        <div>
+            <div>{counter}</div>
+            <div>state:{counter2}</div>
+            <button onClick={increase}>클릭!</button>
+        </div>
+    );
 }
 
 export default App;
