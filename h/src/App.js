bec7355c3341e94253beb1458c871bc0c74f9107
Molkang <mitier26@gmail.com>
2024-03-25 23:37:28 +0900

    HnM 브랜드 페이지

---
+++ h/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import ProductAll from './page/ProductAll';
+import ProductDetail from './page/ProductDetail';
+import Login from './page/Login';
+import Navbar from './components/Navbar';
+
+// 1. 전체상품페이지, 로그인, 상품상세페이지
+// 1-1. 네이게이션 바
+// 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다.
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+// 4. 상품디테일을 눌렀으나, 로드인이 안되있을 겅우에는 로그인 페이지가 엄저 나온다.
+// 5. 로그인 이 되어 있을 경우에는 상품 디테일 페이지를 볼 수 있다.
+// 6. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
+// 7. 로그아웃이되면 상품 디체일페이지를 볼수없다, 다시 로그인 페이지가 보인다.
+// 8. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
+// 9. 상품을 검색 할 수 있다.
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<ProductAll />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/detail:id" element={<ProductDetail />}></Route>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
