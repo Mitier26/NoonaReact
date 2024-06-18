afc6733830152df8da1441845257c95209a1b134
Molkang <mitier26@gmail.com>
2024-06-18 18:28:13 +0900

    날씨앱

--- weather-app2/src/components/WeatherButton.js
+++ weather-app2/src/components/WeatherButton.js
@@ -4,10 +4,10 @@ import { Button } from 'react-bootstrap';
 const WeatherButton = ({ cites, setCity, selCity }) => {
     return (
         <div>
-            <Button variant={`${selCity == '' ? "outline-warning" : "waring"}`} onClick={() => setCity('')}>현재위치</Button>
+            <Button variant={`${selCity === '' ? "outline-warning" : "waring"}`} onClick={() => setCity('')}>현재위치</Button>
 
             {cites.map((city, index) => (
-                <Button variant={city == selCity ? "outline-warning" : "warning"} key={index} onClick={() => setCity(city)}> {city} </Button>
+                <Button variant={city === selCity ? "outline-warning" : "warning"} key={index} onClick={() => setCity(city)}> {city} </Button>
             ))}
         </div>
     )
